19122a115aefa37c64d401d7d31e3c3f123ed3cd
nathaazevedo <natha.azevedo@hotmail.com>
2022-10-10 17:09:52 -0300

    Conceitos de abstração, herença e polimorfismo implementados

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456789", "Modelo 1", "123123123", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\nSmartphone Iphone:");
+Smartphone iphone = new Iphone("123456789", "Modelo 1", "123123123", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
